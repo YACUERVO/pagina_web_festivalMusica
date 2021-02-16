91eee260a2dc72519e0b284de6dc7e910948a214
YAMITH ARLEY CUERVO CORREDOR <yamithcuervo@gmail.com>
2021-02-16 17:11:33 -0500

    proyecto gulp 6

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,4 +1,17 @@
-console.log('listo');
+document.addEventListener('DOMContendLoades', function() {
+    ScrollNav()
+});
+
+function ScrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion__principal a');
+
+    enlaces.forEach(function(enlaces) {
+        console.log(enlaces)
+    })
+
+
+
+};
 document.addEventListener('DOMContentLoaded', function() {
     crearGaleria();
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-console.log('listo');
+document.addEventListener('DOMContendLoades', function() {
+    ScrollNav()
+});
+
+function ScrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion__principal a');
+
+    enlaces.forEach(function(enlaces) {
+        console.log(enlaces)
+    })
+
+
+
+};
